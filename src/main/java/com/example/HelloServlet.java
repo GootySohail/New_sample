fbc148f4f01341814bf47c10dd04650fc6fc3d83
GootySohail <109505341+GootySohail@users.noreply.github.com>
2024-08-03 22:44:49 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
+public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello, World!</h1>");
     }
 }
-
